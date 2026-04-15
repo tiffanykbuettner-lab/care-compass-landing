667c2c23b95947a1596ecead60936f6188879be2
Tiffany Strother <tiffanykbuettner@gmail.com>
2026-04-15 13:56:14 -0500

    fix sitemap robots.txt error

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     sitemap({
       hostname: 'https://www.joincarecompass.com',
+      generateRobotsTxt: false,
       routes: [
         '/',
         '/compass',
